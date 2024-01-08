f63a19f27d16bbe24af8bb3cc37ce19eb1262886
Issa <isaiahabdulrahman@gmail.com>
2024-01-08 12:51:02 +0200

    finish the authroute

---
+++ Back-End/routes/AuthRoute.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { authenticate } from '../middlewares/authMiddleware.js';
+import{
+    signup,
+    login,
+    loggedInUser,
+} from '../controllers/AuthController.js';
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/login-user', authenticate,loggedInUser);
+
+export default router;
